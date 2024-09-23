307751649c9b30e85ccc963a1941083dd53fdf64
chloe <chloesapagepersonal@gmail.com>
2024-09-23 17:58:36 +0100

    Move api call into separate file

--- src/NewGame.tsx
+++ src/NewGame.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useState } from "react";
+import { register } from "./assets/api.js";
 
 /**
  * This component is a basic MVP of part one of the quickstart. It handles registering your agent and receives a token
@@ -7,36 +8,40 @@ import { useState } from "react"
 
 function NewGame() {
   const [token, setToken] = useState();
-  const [resp, setResp] = useState("");
+  const [resp, setResp] = useState(null);
   const [form, setForm] = useState({ symbol: "", faction: "COSMIC" });
+  const [isAgentCreated, setIsAgentCreated] = useState(false);
 
-  return (<>
-    <h1>New Game</h1>
-    <input name="symbol" value={form.symbol} onChange={(e) => setForm({ ...form, symbol: e.currentTarget.value })} />
-    <input name="faction" value={form.faction} onChange={(e) => setForm({ ...form, faction: e.currentTarget.value })} />
-    <input type="submit" onClick={async () => {
-      const resp = await fetch("https://api.spacetraders.io/v2/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          symbol: form.symbol,
-          faction: form.faction,
-        }),
-      });
+  return (
+    <>
+      <h1>New Game</h1>
+      <input
+        name="symbol"
+        value={form.symbol}
+        onChange={(e) => setForm({ ...form, symbol: e.currentTarget.value })}
+      />
+      <input
+        name="faction"
+        value={form.faction}
+        onChange={(e) => setForm({ ...form, faction: e.currentTarget.value })}
+      />
+      <input
+        type="submit"
+        onClick={async () => {
+          const json = await register(form.symbol, form.faction);
 
-      const json = await resp.json();
+          if (json.ok) {
+            setToken(json.body.data.token);
+            setResp(json.body);
+            setIsAgentCreated(true);
+          }
+        }}
+      />
 
-      if (resp.ok) {
-        setToken(json.data.token)
-      }
-
-      setResp(JSON.stringify(json, null, 2))
-    }} />
-    <pre>API token: {token}</pre>
-    <pre>Response: {resp}</pre>
-  </>)
+      <pre>API token: {token}</pre>
+      <pre>Response: {JSON.stringify(resp, null, 2)}</pre>
+    </>
+  );
 }
 
-export default NewGame
+export default NewGame;
---
+++ steps.md
@@ -0,0 +1,33 @@
+# Steps taken and thought processes
+
+1. Initial Commit and Repo rules
+
+Add rule to Github repo to prevent commits to main and require PR.
+
+2. Read and understand code
+
+Make a request to the register endpoint, store the response and token in state.
+
+3. Change page title
+
+could add a favicon but i cba
+
+4. Move api calls into 1 file
+
+move given anonymous function into a new function called register. Need to return the ok response from fetch, as well as the body.
+
+
+
+
+5. Render information from request
+
+Create new components that are conditionally rendered which contain information needed.
+
+NewGame will be the top level component, I could make app the top level component but I'm unsure if that is correct.
+
+
+6. Find starting location
+
+I'm thinking this should be a new view, so maybe incorporate react-router.
+
+6.
